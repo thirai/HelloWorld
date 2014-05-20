ed0ddd26bddcf150acb28288d407862879598db3
Tomohiro HIRAI <thirai@tokaisoftware.co.jp>
2014-05-20 22:39:16 +0900

    second commit

---
+++ src/jp/co/tokaisoftware/Hello.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package jp.co.tokaisoftware;
+
+/**
+ * @author hirai
+ *
+ */
+public class Hello {
+
+	/**
+	 * 
+	 */
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
